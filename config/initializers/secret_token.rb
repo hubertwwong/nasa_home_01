5fafaff1d95f11ff515dcd0ad6427097caad82a5
Hubert Wong <hubertwwong@gmail.com>
2013-10-04 19:44:32 -0700

    intial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NasaHome01::Application.config.secret_key_base = '83a69dcbee9ed295ba1894649dd103636b3acf2004420f6c552ea234395bf11f0fc791430c25353288cf64aaddeddec04e45935e3a52b246a0b89f79771f08ad'
