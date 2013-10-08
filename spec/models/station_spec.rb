359c139dbdffa57cb49c7f972b4e8b1fe6cc6cda
Hubert Wong <hubertwwong@gmail.com>
2013-10-08 00:45:23 -0700

    changed the specs so its in rspec formats.

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -3,12 +3,18 @@ require "spec_helper"
 describe Station do
   
   describe "basic db" do
-    it "write something" do
+    it "write 1 thing" do
       station_one = Station.new
       station_one.name = "mir"
       station_one.description = "russian space station"
       station_one.save
-      assert_equal 1, Station.all.count
+      
+      Station.all.count.should be > 1
+    end
+    
+    it "read station1" do
+      station_one = Station.find_by first_name: 'station1'
+      station_one.description.should be 'station 2 description'
     end
   end
   
